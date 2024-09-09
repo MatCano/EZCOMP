9f88c86709970c295f1eb68a6fcaa453e5a229ea
Fabio Cachone <cachonefabio@gmail.com>
2024-09-09 15:02:29 -0300

    feat: rename destiny

--- app/.antlr/IsiLangParser.java
+++ app/.antlr/IsiLangParser.java
@@ -1,4 +1,4 @@
-// Generated from f:\UFABC\Github\isilanguage-compiler\app\IsiLang.g4 by ANTLR 4.9.2
+// Generated from f:\UFABC\Github\ez-comp\app\IsiLang.g4 by ANTLR 4.9.2
  
 
 from isiExceptions import IsiSemanticException
